53784e200e240ecd025b95f2b6f8f4557521b24d
Ryan Windle <tus27108@temple.edu>
2026-01-29 12:16:13 -0500

    Somewhat successfully made a layout using Linear. Heavy on the somewhat.

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,4 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val button = Button(this)
+
+
+
 }
